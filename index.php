c91fc9cee9d2079287ba419227ca755bcd59ad8e
Fredric Castelius <fredric@castelius.se>
2014-11-28 13:56:05 +0100

    Grand Final

--- index.php
+++ index.php
@@ -2,17 +2,6 @@
 
 	<main>
 
-		<h2>Another damn framework</h2>
-		<p>Seriously, there are great frameworks out there that are kinda all built the same way but with different styles and with a lot of classes in the markup. Lumbr is not just another framework, it's another way of thinking in the crafting and coding process. Lumbr is for you who want everything beautifully taken care of but still create whatever you want without limitations.</p>
-		
-		<p>The most powerful about lumbr is the layout mixin. Check this out.</p>
-
-		<p>This:</p>
-
-<pre class="snippet"><span>@include layout(</span><span class="comment">center, center, normal, full-one-fifth, 1.5rem, 1rem, auto, 0rem</span><span>);</span></pre>
-		
-		<p>Generates this:</p>
-
 		<section id="layout-example">
 			<div>
 				<div>
@@ -36,6 +25,17 @@
 				</div>
 			</div>
 		</section>
+		
+		<h2>Another damn framework</h2>
+		<p>Seriously, there are great frameworks out there that are kinda all built the same way but with different styles and with a lot of classes in the markup. Lumbr is not just another framework, it's another way of thinking in the crafting and coding process. Lumbr is for you who want everything beautifully taken care of but still create whatever you want without limitations.</p>
+		
+		<p>The most powerful about lumbr is the layout mixin. Check this out.</p>
+
+		<p>This:</p>
+
+<pre class="snippet"><span>@include layout(</span><span class="comment">center, center, normal, full-one-fifth, 1.5rem, 1rem, auto, 0rem</span><span>);</span></pre>
+		
+		<p>Generates this:</p>
 		<p> </p>
 		<h3>Alright, so what happen</h3>
 		<p>With just one line of code we made a responsive / elastic layout which has 2 columns. One that is 75% and the other the remaining 25%. Both automatically stacks and becomes 100% width on smaller devices. We also squeese in some vertical and horisontal padding. On top of that we verticlly center the sidebar to the main column. This is your new way of coding layouts.</p>
